2445b7f0845667e1dc5f5e0c8bed50a194da8f3e
sc4red2 <165446983+sc4red2@users.noreply.github.com>
2024-03-30 06:44:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const container = document.getElementById('container-games');
-const searchInput = document.getElementById('search');
+const searchInput = document.getElementById('search'); 
 
 const GAMES_LIMIT = 12;
 let games = [];
-let filteredGames = [];
-let tipoDeJuego = "pcGames"; // Valor por defecto
+let filteredGames = []; 
 
 // Función para obtener los datos de los videojuegos
 async function fetchGames() {
@@ -22,8 +21,8 @@ async function fetchGames() {
 
 // Función para renderizar los videojuegos
 function renderGames(gamesToRender) {
-  container.innerHTML = '';
-  for (let i = 0; i < GAMES_LIMIT && i < gamesToRender.length; i++) {
+  container.innerHTML = ''; 
+  for (let i = 0; i < GAMES_LIMIT; i++) {
     const gameCard = document.createElement('div');
     gameCard.classList.add('game-card');
     gameCard.innerHTML = `
@@ -35,31 +34,12 @@ function renderGames(gamesToRender) {
   }
 }
 
-// Función para filtrar los videojuegos por plataforma
-function filterByPlatform() {
-  if (tipoDeJuego === 'uncrackedGames') {
-    filteredGames = games.filter((game) => game.platform === 'Uncracked');
-  } else {
-    filteredGames = games.filter((game) => game.platform === tipoDeJuego);
-  }
-  renderGames(filteredGames);
-}
-
-// Event listeners para los botones de plataforma
-const platformButtons = document.querySelectorAll('.platform-button');
-platformButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    tipoDeJuego = event.target.value;
-    filterByPlatform();
-  });
-});
-
-// Función para filtrar los videojuegos por búsqueda
+// Función para filtrar los videojuegos
 function filterGames() {
   filteredGames = games.filter((game) =>
     game.title.toLowerCase().includes(searchInput.value.toLowerCase())
   );
-  renderGames(filteredGames);
+  renderGames(filteredGames); // Renderizar los videojuegos filtrados
 }
 
 fetchGames()
@@ -69,6 +49,6 @@ fetchGames()
   })
   .catch((error) => {
     console.error('Error fetching games:', error);
-});
+  });
 
 searchInput.addEventListener('keyup', filterGames);
