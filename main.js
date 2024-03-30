0370daaaa91cdbc4c07dd489e33cfdabe288ba3a
danielquirozc <142172231+danielquirozc@users.noreply.github.com>
2024-03-30 01:50:54 -0400

    Script del filtrado y atributos value de los botones de la barra de navegación.

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
 const container = document.getElementById('container-games');
 const searchInput = document.getElementById('search'); 
+const botones = document.getElementsByClassName('platform-button');
 
 const GAMES_LIMIT = 12;
 let games = [];
 let filteredGames = []; 
 
+let tipoDeJuego = "pcGames";
+
+Object.values(botones).forEach((boton) => {
+  boton.addEventListener(('click'), (event) => {
+    tipoDeJuego = event.target.value
+    fetchGames()
+  })
+})
+
+
 // Función para obtener los datos de los videojuegos
 async function fetchGames() {
   try {
-    const httpResponse = await fetch('games.json');
+    const httpResponse = await fetch(`gamesData/${tipoDeJuego}.json`);
     if (!httpResponse.ok) {
       throw new Error(`HTTP error! status: ${httpResponse.status}`);
     }
     const jsonData = await httpResponse.json();
-    return jsonData;
+    games = jsonData;
+    renderGames(games);
   } catch (error) {
     console.error('Error fetching games:', error);
   }
 }
+
+fetchGames();
 
 // Función para renderizar los videojuegos
 function renderGames(gamesToRender) {
@@ -41,14 +55,5 @@ function filterGames() {
   );
   renderGames(filteredGames); // Renderizar los videojuegos filtrados
 }
-
-fetchGames()
-  .then((data) => {
-    games = data;
-    renderGames(games);
-  })
-  .catch((error) => {
-    console.error('Error fetching games:', error);
-  });
 
 searchInput.addEventListener('keyup', filterGames);
